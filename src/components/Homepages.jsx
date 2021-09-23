ba3e04c9501a48887f34daa30674859e0122ce3f
Manohar Prabhu R <rmprabhu@hotmail.com>
2021-09-23 12:53:56 +0530

    Implemented Services, Redux, and display the same in homepage along with millify.

--- src/components/Homepages.jsx
+++ src/components/Homepages.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import millify from "millify";
-import { Typography, Row, Col, Statistic } from "antd";
+import { Typography, Row, Col, Statistic, Spin } from "antd";
 import { Link } from "react-router-dom";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 
 const Homepages = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
+  if (isFetching) return <Spin />;
+
+  console.log(data);
+
   return (
     <>
       <Title level={2} className="heading">
@@ -13,19 +21,31 @@ const Homepages = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Curriences" value="5" />
+          <Statistic title="Total Curriences" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="24h Markets" value="5" />
+          <Statistic
+            title="24h Markets"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '177d42a19fmshac5d3ce80c76a32p1318b0jsn785690e15d62'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+}  = cryptoApi;
+
